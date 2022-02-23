c3ae5b8faa3f9242b7f99d3665f9cb08df07fa95
LianMejia <lianmejiag@gmail.com>
2022-02-22 21:37:32 -0500

    Se implemento provider y se maneja el estado de la barra de navegacion

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
+    return ChangeNotifierProvider(
+      create: (_) => _NavegaconModel(),
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
     );
   }
 }
 
 class _Navegacion extends StatelessWidget {
-  const _Navegacion({
-    Key? key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegaconModel>(context);
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual = i,
       items: [
         BottomNavigationBarItem(
             icon: Icon(Icons.person_outline), label: 'Para ti'),
@@ -31,7 +33,9 @@ class _Navegacion extends StatelessWidget {
 class _Paginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegaconModel>(context);
     return PageView(
+      controller: navegacionModel.pageController,
       //Este physics impide que al deslizarse mueva la pantallla
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
@@ -42,4 +46,18 @@ class _Paginas extends StatelessWidget {
       ],
     );
   }
+}
+
+class _NavegaconModel with ChangeNotifier {
+  int _paginaActual = 0;
+  PageController _pageController = PageController(initialPage: 0);
+  int get paginaActual => _paginaActual;
+  set paginaActual(int valor) {
+    _paginaActual = valor;
+    _pageController.animateToPage(valor,
+        duration: Duration(milliseconds: 250), curve: Curves.easeOut);
+    notifyListeners();
+  }
+
+  PageController get pageController => _pageController;
 }
